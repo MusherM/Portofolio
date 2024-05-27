f96a4b5446de5bbd27bfe22597d867f13a6c06a1
Musher <593905676@qq.com>
2024-05-27 21:49:03 +0800

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/Portofolio" : "",
+  assetPrefix: isProd ? "/Portofolio/" : "",
   images: {
     unoptimized: true,
-    loader: "imgix",
-    path: "https://musherm.github.io/Portofolio/",
   },
 };
 
