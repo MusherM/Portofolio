46e9f39fb3dcb279b2ede55e7591ee7a81941f02
Musher <593905676@qq.com>
2024-05-27 20:57:47 +0800

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,8 @@
 /** @type {import('next').NextConfig} */
-// 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
-// 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = `https://musherm.github.io/Portofolio/`;
-
-// 用于为应用设置基础路径
-// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-let basePath = `https://musherm.github.io/Portofolio`;
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `https://musherm.github.io/Portofolio/`;
-  basePath = `https://musherm.github.io/Portofolio`;
-}
 
 const nextConfig = {
-  assetPrefix,
-  basePath,
+  basePath: "/Portofolio",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
