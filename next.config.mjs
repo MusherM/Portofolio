c5ff08a14a2c37b239fd53292e554a9e41f43788
Musher <593905676@qq.com>
2024-05-27 21:21:43 +0800

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/Portofolio" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
