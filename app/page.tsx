eb04826200e5e7c8d48c750a0c7fd6c5b6a8d31e
Musher <593905676@qq.com>
2024-06-20 17:15:35 +0800

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Approach from '@/components/Approach'
 import Clients from '@/components/Clients'
-import Experience from '@/components/Experience'
 import Footer from '@/components/Footer'
 import Grid from '@/components/Grid'
 import Hero from '@/components/Hero'
 import RecentProjects from '@/components/RecentProjects'
 import { FloatingNav } from '@/components/ui/FloatingNav'
 import { navItems } from '@/data'
-import Image from 'next/image'
-import { FaHome } from 'react-icons/fa'
 
 export default function Home() {
   return (
@@ -25,4 +22,10 @@ export default function Home() {
       </div>
     </main>
   )
+}
+
+export const metadata = {
+  icons: {
+    icon: '/next.svg'
+  }
 }
